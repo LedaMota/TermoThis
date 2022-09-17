523451256e9895f484debff5b6893fc87938cf72
Samuel Ribeiro Batista <leidymotastz@outlook.com>
2022-09-17 17:33:59 -0300

    first commit

---
+++ Acessar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermoThis
+{    //Aqui criei uma classe Acessar 
+    class Acessar
+    {   //Aqui declarei uma variavel do tipo string senha fiz uma atribuição de senha.
+        string senha = "abc123";
+                
+        //Aqui criei um metódo tipo bool com o nome login  com um parametro que vai receber senha do tipo string
+        public bool Login(string senha)
+        {
+            //Aqui o retorno do metódo onde eu comparei a senha vindo da variavel com a senha vindo do parametro.
+            //coloquei o this para especificar que estou pegando a variavel e não o parametro.
+            return this.senha == senha;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermoThis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Aqui estou intanciando a classe e passando um objeto
+            Acessar a = new Acessar();
+
+
+            //Aqui montei uma condicional pegando um metodo login e estou passando uma senha,
+            //se for igual entra dentro da condicional se não for retorna false.
+            if (a.Login("abm999"))
+            {
+                Console.WriteLine("Seja bem vindo!");
+            }
+            else
+            {
+                Console.WriteLine("Acesso negado!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
